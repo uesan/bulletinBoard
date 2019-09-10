96eda88404b7e7364c3a74bcbbfb6656eb21b1cb
koutaro <kotaro0310ilikeblue@gmail.com>
2019-09-10 18:16:42 +0900

    Board,Commentモデルの作成  もろもろの初期設定  とりあえずindexのみハリボテを作成

--- bulletinBoard/models.py
+++ bulletinBoard/models.py
@@ -3,9 +3,37 @@ from django.db import models
 # Create your models here.
 
 class Board(models.Model):
+    '''
+    掲示板の「板」にあたる部分のモデル。
+    :parameter
+    ----------
+    title : django.db.models.CharField
+        板のタイトル
+
+    pub_date : django.db.models.DateTimeField
+        チュートリアルと同じく、掲示される日時
+
+    '''
     title = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('dete published')
 
-class comment(models.Model):
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    '''
+    「板」につくコメントにあたるモデル
+    :parameter
+    ----------
+    text : django.db.models.TextField
+        コメント本文
+
+    board : django.db.models.ForeignKey(this.Board)
+        どの「板」のコメントかを判断する変数
+
+    '''
     text = models.TextField(max_length = 400)
-    Board = models.ForeignKey(Board, on_delete = models.CASCADE)
+    board = models.ForeignKey(Board, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.text
--- bulletinBoard/views.py
+++ bulletinBoard/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views import View
 
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'bulletinBoard/index.html')
+"""
+本によると、まずは基本汎用ビューを用いて作ってみようとのことなので作ってみます
+"""
 
+class BoardIndexView(View):
+    '''
+    掲示板の一覧を表示するViewになる（予定）
+    '''
+    def get(self, request, *args, **kwargs):
+        '''
+        GETリクエスト用のメソッド
+        *argsと**kwargsを引数にとるが、こいつらの存在意義がよくわかっていない。
+        リクエスト以外でのデータや文字列の受け取りが発生する・・・？
+        '''
+        return render(request, 'bulletinBoard/index.html')
 
-from django.shortcuts import render
+index = BoardIndexView.as_view()
 
 # Create your views here.

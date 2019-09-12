6c40ae9733c2fbe31eff23876041cdd5451feb93
koutaro <kotaro0310ilikeblue@gmail.com>
2019-09-12 14:13:39 +0900

    ログイン、ログアウト機能の実装 ほぼ教科書の写しですが完成

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('bulletinBoard/', include('bulletinBoard.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
